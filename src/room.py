fadc5d5229ed8ee2436da834d7eda0b84d2b4b07
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-29 16:50:38 -0500

    Define a Room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,9 @@
-# Implement a class to hold room information. This should have name and
-# description attributes.
+class Room:
+    """The room class contains a constructor to instanciate a new room.
+    The room contains a method to print it's description. Trigger the
+    method on this room when a player enters"""
+
+    def __init__(self, name, description, inventory=None):
+        self.name = name
+        self.description = description
+        self.inventory = inventory
