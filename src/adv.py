5c560fb67c2d74807bb926a23c656df21c779cde
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-29 17:10:01 -0500

    Handle attempted moves

--- src/adv.py
+++ src/adv.py
@@ -55,9 +55,20 @@ def bad_move():
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+
+def attempt_move(player, direction):
+    print("\nAttempted to move... \n")
+    attribute = direction + "_to"
+
+    if hasattr(player.location, attribute):
+        next_room = getattr(player.location, attribute)
+        player.location = next_room
+        return
+
+    else:
+        bad_move()
+
+
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
