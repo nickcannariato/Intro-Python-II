c1d411441d8648b36d5dc0837a7d2e97b01b2c85
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-30 15:38:01 -0500

    Add a Player inventory
    Signed-off-by: Nick Cannariato <434063+nickcannariato@users.noreply.github.com>

--- src/player.py
+++ src/player.py
@@ -1,11 +1,14 @@
 class Player:
     """An object to represent the Player
-    
+
     Args:
         name: String -- The name of the Player
         current_room: String -- the current Room the player is in
+        [inventory]: List -- The items a player is holding
     """
 
-    def __init__(self, name, current_room):
+    def __init__(self, name: str, current_room: str, inventory: list = None):
         self.name = name
         self.current_room = current_room
+        self.inventory = inventory
+
