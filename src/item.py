0c102f9dd42ea1c1a1ccd3c6391b26ca3586cbb9
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-05-30 17:38:37 -0500

    Add on_take and on_drop methods to Item
    Signed-off-by: Nick Cannariato <434063+nickcannariato@users.noreply.github.com>

--- src/item.py
+++ src/item.py
@@ -10,5 +10,14 @@ class Item:
         self.name = name
         self.description = description
 
+    def __repr__(self):
+        return f"{self.name}"
+
     def __str__(self):
-        return f"{self.name}\n{self.description}"
+        return f"{self.name} -- {self.description}"
+
+    def on_take(self):
+        print(f"You have picked up {self.name}")
+
+    def on_drop(self):
+        print(f"You have dropped {self.name}")
